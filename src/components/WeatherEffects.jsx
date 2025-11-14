208cf7872e7bbdbe2a013135bfb7d9476bd8d473
esther-joseph <esther8joseph@gmail.com>
2025-11-14 16:25:27 -0600

    loading issue with rain toggle

--- src/components/WeatherEffects.jsx
+++ src/components/WeatherEffects.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo, useEffect } from 'react'
+import React, { useRef, useMemo, useEffect, useCallback } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { Icosahedron, Sphere } from '@react-three/drei'
 import * as THREE from 'three'
